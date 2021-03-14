fa9efa45fa5759070563cab526607d1ecacb6288
i-kostenevich <kostenevich9696@gmail.com>
2021-03-14 21:43:33 +0200

    changed logic for breakpoints el-marks element

--- src/App.vue
+++ src/App.vue
@@ -44,13 +44,14 @@
             </div>
             <el-slider
               v-model="creditTerm"
-              :max="216"
+              :max="228"
               :min="12"
               :step="12"
               :show-tooltip="false"
               label="Срок кредита"
               :marks="dataMarks.marks"
-              class="el-slider-marks"
+              :class="[{ 'el-slider-marks-mobile': handlerResize }]"
+              id="el-slider-marks"
             ></el-slider>
           </div>
         </div>
@@ -62,22 +63,29 @@
             :key="bank.name"
             :class="{ active: bank.isActive }"
             @active-bank="isActiveBank(bank)"
-          >{{ bank.name }}</app-select-bank-item>
+            >{{ bank.name }}</app-select-bank-item
+          >
         </div>
       </div>
       <div class="content__column">
         <div class="show-items">
           <div class="show-item">
             <div class="show-item__label">Сумма кредита</div>
-            <div class="show-item__result">{{ currency(totalAmountOfCredit) }}</div>
+            <div class="show-item__result">
+              {{ currency(totalAmountOfCredit) }}
+            </div>
           </div>
           <div class="show-item">
             <div class="show-item__label">Ежемесячный платеж</div>
-            <div class="show-item__result">{{ currency(totalMounthlyPayment) }}</div>
+            <div class="show-item__result">
+              {{ currency(totalMounthlyPayment) }}
+            </div>
           </div>
           <div class="show-item">
             <div class="show-item__label">Рекомендуемый доход</div>
-            <div class="show-item__result">{{ currency(totalRecommendedIncome) }}</div>
+            <div class="show-item__result">
+              {{ currency(totalRecommendedIncome) }}
+            </div>
           </div>
         </div>
       </div>
@@ -119,7 +127,8 @@ export default {
           180: '180',
           192: '192',
           204: '204',
-          216: '216'
+          216: '216',
+          228: '228'
         },
         arrMarksItems: [],
         arrMarksDistance: []
@@ -131,7 +140,10 @@ export default {
         { name: 'ОТП банк', rate: 7.1, isActive: false },
         { name: 'Ощадбанк', rate: 7.3, isActive: false }
       ],
-      currency
+
+      currency,
+      windowWidth: window.innerWidth,
+      abc: 0
     }
   },
 
@@ -146,36 +158,87 @@ export default {
     }
   },
 
+  computed: {
+    handlerResize () {
+      // if (this.windowWidth < 550) {
+      //   return true
+      // }
+      return this.windowWidth < 550
+    }
+  },
+
   mounted () {
-    this.dataMarks.arrMarksItems.push(...document.querySelectorAll('.el-slider__marks-stop'))
-    this.dataMarks.arrMarksItems.forEach((item) => {
-      const getStyleMarksDistance = parseFloat(getComputedStyle(item).left)
-      this.dataMarks.arrMarksDistance.push(getStyleMarksDistance)
+    this.dataMarks.arrMarksItems.push(
+      ...document.querySelectorAll('.el-slider__marks-stop')
+    )
+
+    this.dataMarks.arrMarksDistance = this.dataMarks.arrMarksItems.map((item) => ({ item }))
+    const arrMarksMarks = Object.values(this.dataMarks.marks).map((item) => +item)
+    this.dataMarks.arrMarksDistance.forEach((item, idx) => {
+      item.marksValue = arrMarksMarks[idx]
     })
+
+    console.log(this.dataMarks.arrMarksDistance)
+
+    // const getMarksDistanceFn = () => {
+    //   this.dataMarks.arrMarksItems.forEach(item => {
+    //     const getStyleMarksDistance = parseFloat(getComputedStyle(item).left)
+    //     this.dataMarks.arrMarksDistance.push(getStyleMarksDistance)
+    //   })
+    // }
+    // getMarksDistanceFn()
+
+    window.onresize = (e) => {
+      this.windowWidth = window.innerWidth
+
+      // this.dataMarks.arrMarksDistance.length = 0
+      // getMarksDistanceFn()
+    }
+
+    // this.dataMarks.arrMarksItems = [...document.querySelectorAll('.el-slider__marks-stop')]
+
+    // const aa = Object.values(this.dataMarks.marks) !
+
+    // this.dataMarks.arrMarksDistance = Object.values(this.dataMarks.arrMarksItems).map(item => ({mark: item, isDone: false})) !
+  },
+
+  watch: {
+    creditTerm (elem) {
+      this.dataMarks.arrMarksDistance.forEach((item, idx) => {
+        if (elem >= item.marksValue) {
+          item.item.classList.add('done')
+        } else {
+          item.item.classList.remove('done')
+        }
+        // console.log(item.marksValue);
+      })
+      console.log(this.dataMarks.arrMarksDistance)
+    }
   },
 
   updated () {
     // active breakpoint logic for creditTerm input
-    if (document.querySelector('.el-slider-marks')) {
-      const btnSlider = document.querySelector('.el-slider-marks .el-slider__button-wrapper')
-      const getStyleButtonDistance = parseFloat(getComputedStyle(btnSlider).left)
+    if (document.querySelector('#el-slider-marks')) {
 
-      const arrMarksDone = this.dataMarks.arrMarksItems.filter((item, idx) => {
-        if (idx > this.dataMarks.arrMarksDistance.indexOf(getStyleButtonDistance)) {
-          item.classList.remove('done')
-        }
-        return idx <= this.dataMarks.arrMarksDistance.indexOf(getStyleButtonDistance)
-      })
-
-      arrMarksDone.forEach((item) => {
-        item.classList.add('done')
-      })
+      // const ss = Object.values(this.dataMarks.marks).map(item => console.log(+item))
+      // const btnSlider = document.querySelector('.el-slider-marks .el-slider__button-wrapper')
+      // // const getStyleButtonDistance = parseFloat(getComputedStyle(btnSlider).left)
+      // this.abc = parseFloat(getComputedStyle(btnSlider).left)
+      // const arrMarksDone = this.dataMarks.arrMarksItems.filter((item, idx) => {
+      //   if (idx > this.dataMarks.arrMarksDistance.indexOf(this.abc)) {
+      //     item.classList.remove('done')
+      //   }
+      //   return idx <= this.dataMarks.arrMarksDistance.indexOf(this.abc)
+      // })
+      // arrMarksDone.forEach(item => item.classList.add('done'))
     }
 
     // calculator logic
     this.totalAmountOfCredit = this.totalCost - this.initialFee
-    this.totalMounthlyPayment = Math.floor((this.totalAmountOfCredit + (((this.totalAmountOfCredit / 100) * this.percent) / 12) * this.creditTerm) / this.creditTerm)
-    this.totalRecommendedIncome = this.totalMounthlyPayment + ((this.totalMounthlyPayment / 100) * 35)
+    this.totalMounthlyPayment = Math.round(
+      (this.totalAmountOfCredit + (((this.totalAmountOfCredit / 100) * this.percent) / 12) * this.creditTerm) / this.creditTerm
+    )
+    this.totalRecommendedIncome = this.totalMounthlyPayment + (this.totalMounthlyPayment / 100) * 35
 
     if (this.totalCost < this.initialFee) {
       this.initialFee = this.totalCost
@@ -191,15 +254,22 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@/assets/styles/main.scss';
-// @import './assets/styles/element-ui.css';
+@import '@/assets/styles/scss/main.scss';
 
 .content {
   padding-top: 80px;
   display: flex;
 
-  .content__column {
+  @media screen and (max-width: 550px) {
+    padding-top: 50px;
+  }
+
+  &__column {
     padding: 0 20px;
+
+    @media screen and (max-width: 550px) {
+      padding: 0 10px;
+    }
 
     &:first-child {
       flex: 1 1 100%;
@@ -228,6 +298,12 @@ export default {
       font-size: 24px;
     }
   }
+
+  @media (max-width: 550px) {
+    margin: 30px 0px 0px 0px;
+    border-top: 2px solid #e5e5e5;
+    padding-top: 30px;
+  }
 }
 
 .content__bank-selection {
@@ -239,95 +315,12 @@ export default {
   }
 }
 
-.el-slider {
-  padding: 0 10px;
+@media (max-width: 860px) {
+  .content {
+    flex-direction: column;
 
-  .el-slider__runway {
-    height: 20px; // height for main line
-    margin: 23px 0 16px 0;
-    background-color: #e4e7ed;
-    border-radius: 15px;
-    position: relative;
-    cursor: pointer;
-    vertical-align: middle;
-    box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.25);
-
-    .el-slider__bar {
-      height: 20px; // height for active line
-      background-color: #1adf2e;
-      border-radius: 15px;
-      position: absolute;
-    }
-
-    .el-slider__button-wrapper {
-      height: 36px;
-      width: 36px;
-      position: absolute;
-      z-index: 1;
-      top: -8px;
-      transform: translateX(-50%);
-      background-color: #fff;
-      text-align: center;
-      border-radius: 50%;
-      box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.4);
-
-      .el-slider__button {
-        display: inline-block;
-        width: 16px;
-        height: 16px;
-        background-color: #1adf2e;
-        vertical-align: middle;
-        border-radius: 50%;
-        box-sizing: border-box;
-        transition: 0.2s;
-      }
-
-      &:after {
-        display: inline-block;
-        content: '';
-        height: 100%;
-        vertical-align: middle;
-      }
-    }
-
-    .el-slider__stop.el-slider__marks-stop {
-      position: absolute;
-      height: 26px;
-      width: 26px;
-      border-radius: 50%;
-      background-color: #e5e5e5;
-      transform: translate(-50%, -11%);
-
-      &.done {
-        background: #1adf2e;
-      }
-
-      &:after {
-        content: '';
-        width: 12px;
-        height: 12px;
-        background: #fff;
-        display: inline-block;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: 50%;
-      }
-    }
-
-    .el-slider__stop {
-      background-color: #000;
-    }
-
-    .el-slider__marks {
-      .el-slider__marks-text {
-        position: absolute;
-        transform: translateX(-50%);
-        font-size: 14px;
-        color: #000;
-        margin-top: 40px;
-      }
+    &__column:first-child {
+      border: none;
     }
   }
 }
